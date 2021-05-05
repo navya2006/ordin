ae78ac40d1931a944f488a6616077e47559001c4
navya2006 <navya2006.github.com>
2021-05-05 11:38:21 +0530

    js code now working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
  // color changing navbar
  jQuery(document).on("scroll", function () {
-    if ($(document).scrollTop() > 50) {
-        $(".navb").css("background-color","#000");
+    if ($(document).scrollTop() > 100) {
+        $(".navb").css("background-color","rgba(0,0,0,0.6)");
     } else {
         $(".navb").css("background-color","transparent");
     }
